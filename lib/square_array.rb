9769552b2e32d9b517c008f3dac2377dcffe822f
Miwha Geschwind <miwhag@gmail.com>
2019-12-22 15:34:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   counter = 0
  
   while counter <= array.length do
-  puts array[counter] * array[counter]
-        counter += 1
+    puts array[counter] * array[counter]
+    counter += 1
   end
 end
 
